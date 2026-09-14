b63f4b9d9c3226e5d9f583f520ddd96fb8085b27
Dodi Indika <dodiindika12@gmail.com>
2026-09-14 09:02:28 +0800

    fix: import useCallback in MainDashboard.jsx

--- components/MainDashboard.jsx
+++ components/MainDashboard.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { getRoleLevel, isMeetingSchedule, isWorksheetSchedule } from './WeeklyScheduleView';
 
 // Helper format tanggal deadline
